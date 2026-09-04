e4f3cf11fb393ff487c2e557fed1b510f512501c
Satyam Kumar <satyam31sk@gmail.com>
2026-09-04 13:35:27 +0530

    add 27-selection-sort.cpp

--- Unit1/27-selection-sort.cpp
+++ Unit1/27-selection-sort.cpp
@@ -6,27 +6,35 @@ using namespace std;
 int main() {
     // Create the array
     int arr[] = {64, 24, 12, 22, 11};
-    // find the length of array
+
+    // Find length of array
     int len = sizeof(arr) / sizeof(arr[0]);
-    // start traversing the array
+
+    // Traverse the array
     for (int i = 0; i < len - 1; i++) {
-        // find the minimum element in unsorted array
-        int min_index = i;
+
+        // Assume current element is minimum
+        int minIndex = i;
+
+        // Find the smallest element
         for (int j = i + 1; j < len; j++) {
-            if (arr[j] < arr[min_index]) {
-                min_index = j;
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
             }
         }
-        // swap the found minimum element with the first element
-        int temp = arr[min_index];
-        arr[min_index] = arr[i];
-        arr[i] = temp;
+
+        // Swap minimum element with current element
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
-    // print the sorted array
+
+    // Print sorted array
     cout << "Sorted array: ";
+
     for (int i = 0; i < len; i++) {
         cout << arr[i] << " ";
     }
-    cout << endl;
+
     return 0;
 }
